48fb36e6ba96e22fb59f7e2f0af59a36d72ab2cf
Urvish <urvishdabhi1121@gmail.com>
2026-09-19 18:06:22 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
   plugins: [
     react(),
   ],
+  define: {
+    'import.meta.env.VITE_BASE44_APP_ID': JSON.stringify('6aaef4b5d1dd0d215b647eb5'),
+    'import.meta.env.VITE_BASE44_APP_BASE_URL': JSON.stringify('https://app.base44.com'),
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
