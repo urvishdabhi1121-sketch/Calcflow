ffd6ad8ea0d92a3524de510fd633b771a69199d5
Urvish <urvishdabhi1121@gmail.com>
2026-09-19 20:49:49 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,18 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/Calcflow/',
+
   plugins: [
     react(),
   ],
+
   define: {
     'import.meta.env.VITE_BASE44_APP_ID': JSON.stringify('6aaef4b5d1dd0d215b647eb5'),
     'import.meta.env.VITE_BASE44_APP_BASE_URL': JSON.stringify('https://app.base44.com'),
   },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
